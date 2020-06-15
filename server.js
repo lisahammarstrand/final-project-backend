5ec79f751897c5252780108d20fec977d059ed64
Lisa Hammarstrand <lisa@Lisas-Air.lan>
2020-06-15 22:16:56 +0200

    Adjusted model and endpoints

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import crypto from 'crypto'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt-nodejs'
 import testUserData from './data/testuser.json'
-import testTrainingData from './data/testtraining.json'
+/* import testTrainingData from './data/testtraining.json' */
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/finalproject"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -38,25 +38,27 @@ const User = mongoose.model('User', {
   training: {
     type: String,
     required: true
+  },
+  times: {
+    type: Number
   }
 })
 
-
-const TrainingStats = mongoose.model('TrainingStats', {
+/* const TrainingStats = mongoose.model('TrainingStats', {
   times: {
     type: Number,
     required: true
   }
-})
+}) */
 
 if (process.env.RESET_DATABASE) {
   console.log('Resetting database ...')
 
   const seedDatabase = async () => {
     await User.deleteMany()
-    await TrainingStats.deleteMany()
+    /* await TrainingStats.deleteMany() */
     await testUserData.forEach((testUser) => new User(testUser).save())
-    await testTrainingData.forEach((testTraining) => new TrainingStats(testTraining).save())
+    /*     await testTrainingData.forEach((testTraining) => new TrainingStats(testTraining).save()) */
   }
   seedDatabase()
 }
@@ -84,11 +86,13 @@ app.get('/', (req, res) => {
   res.send('Final project backend')
 })
 
+// WORKS - get all users
 app.get('/users', async (req, res) => {
   const users = await User.find().exec()
   res.json(users)
 })
 
+// WORKS - registrer new user
 app.post('/users', async (req, res) => {
   try {
     const { name, email, password, activepackage, training } = req.body
@@ -96,53 +100,64 @@ app.post('/users', async (req, res) => {
     user.save()
     res.status(201).json({ id: user._id, accessToken: user.accessToken })
   } catch (err) {
+    console.log(JSON.stringify(err))
     res.status(400).json({ message: 'Could not create user', errors: err.errors })
   }
 })
 
 /* app.get('/users', authenticateUser) */
 
+// WORKS - find one user
 app.get('/users/:userId', async (req, res) => {
   const userId = req.params.userId
   const userProfile = await User.findOne({ _id: userId }).exec()
   res.json(userProfile)
 })
 
-/* app.post('/sessions', async (req, res) => {
+// WORKS - Update training stats by 1
+app.put('/users/:userId/updatestats', async (req, res) => {
+  const { userId } = req.params
+  const ERR_COULD_NOT_FIND = `Could not find ${userId} to update`
+  try {
+    const updatedStats = await User.updateOne(
+      { _id: userId },
+      { $inc: { times: 1 } }
+    )
+    res.status(201).json(updatedStats)
+  } catch (err) {
+    console.log(JSON.stringify(err))
+    res.status(404).json({ message: ERR_COULD_NOT_FIND })
+  }
+})
+
+// WORKS - log in user
+app.post('/sessions', async (req, res) => {
   const user = await User.findOne({ email: req.body.email })
   if (user && bcrypt.compareSync(req.body.password, user.password)) {
     res.json({ userId: user._id, accessToken: user.accessToken })
   } else {
     res.status(401).json({ notFound: true, statusCode: 401, error: "Login failed" })
   }
-}) */
+})
 
+/* // WORKS
 app.get('/trainingstats', async (req, res) => {
   const trainingStats = await TrainingStats.find().exec()
   res.json(trainingStats)
-})
+}) */
 
+/* // DOES NOT WORK returns empty object BUT POSTS TO DB
 app.post('/trainingstats', async (req, res) => {
-  const { training } = req.body
-  const newTrainingStats = new TrainingStats({ training })
-  newTrainingStats.save()
-  res.status(201).json({ training })
-})
-
-app.put('/trainingstats/:statsId/update', async (req, res) => {
-  const { statsId } = req.params
-  const ERR_COULD_NOT_FIND = `Could not find ${statsId} to update`
   try {
-    const updatedStats = await TrainingStats.updateOne(
-      { _id: statsId },
-      { $inc: { times: 1 } }
-    )
-    res.status(201).json(updatedStats)
+    const { times } = req.body
+    const newTrainingStats = new TrainingStats({ times })
+    newTrainingStats.save()
+    res.status(201).json({ training: training.times })
   } catch (err) {
-    res.status(404).json({ message: ERR_COULD_NOT_FIND })
+    console.log(JSON.stringify(err))
+    res.status(400).json({ message: 'Could not create trainingstats', errors: err.errors })
   }
-})
-
+}) */
 
 
 // Start the server
